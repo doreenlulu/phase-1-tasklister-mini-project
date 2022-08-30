414b6fe9bada5014d5bfd2fea1373abdf46a7994
Doreen <doreenlulu97@gmail.com>
2022-08-30 15:48:54 +0300

    Complted assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+		handleToDolist(e.target.description.value);
+		// console.log(document.getElementById("description").value);
+		form.reset();
+	});
 });
+function handleToDolist(todoitem) {
+	let arr = [];
+	let ul = document.getElementById("tasks");
+	let btn = document.createElement("button");
+	btn.addEventListener("click", deletetodo);
+	btn.textContent = " x ";
+	ul.textContent = `${todoitem}  `;
+	ul.appendChild(btn);
+	arr.push(todoitem);
+	console.log(arr);
+	return arr;
+}
+
+function deletetodo(e) {
+	e.target.parentNode.remove();
+}
+
